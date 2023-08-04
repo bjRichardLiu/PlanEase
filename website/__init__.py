3554090a555cfbc29be013de56c88304219b7f02
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2023-08-04 14:29:40 -0400

    Updated with most of the flask code

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# create a database
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'mysecretkeymysecretkey'
+    # the databse is stored in the same directory as the website folder, the same as __init__.py
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
     
     from .views import views
     from .auth import auth
