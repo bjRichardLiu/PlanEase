2efedc68845c84dee61843c2698743908536b79e
achemai <akemisai111@gmail.com>
2023-08-04 16:43:36 -0400

    html
    home page with a toggle bar that leads to other pages(login, logout etc)

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # split the application into multiple files
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Home</h1>"
+    return render_template("home.html")
