458bd12e1853384a333a0d1db3ac7827eea5e2fe
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2023-08-04 19:18:58 -0400

    Updated with add task

--- website/models.py
+++ website/models.py
@@ -6,14 +6,34 @@ from sqlalchemy.sql import func
 
 
 
-class Tasks (db.Model):
+class Task (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    morning = db.Column(db.Boolean)
+    weekdaysOnly = db.Column(db.Boolean)
+    deadline = db.Column(db.Integer)
+    priority = db.Column(db.Integer)
+    timeRequired = db.Column(db.Integer)
+    
     # foreign key to link to user, if no id, it won't create the task
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-# TODO add other kinds of information of the user
+class ReservedTime (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    beginTime = db.Column(db.Integer)
+    endTime = db.Column(db.Integer)
+    weekdaysOnly = db.Column(db.Boolean)
+    # foreign key to link to user, if no id, it won't create the task
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class WakeUpTime (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    weekdays = db.Column(db.Integer)
+    weekends = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     # create columns for the database
