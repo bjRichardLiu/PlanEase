8001af519ac835e04fae1979ae81aaa6917a00b4
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2023-08-07 18:31:49 -0400

    back to develop mode

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from gevent.pywsgi import WSGIServer
 app = create_app()
 
 if __name__ == '__main__':
-    # app.run(debug=True)
+    app.run(debug=True)
     # Production using wsgi server
-    app.run(host='0.0.0.0:$PORT',port=5000)
+    # app.run(host='0.0.0.0:$PORT',port=5000)
     
