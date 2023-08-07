4331bd6e7e1404bcbd43b3968ae56c386060acac
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2023-08-07 02:42:41 -0400

    Try deployment

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from website import create_app
+from gevent.pywsgi import WSGIServer
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=False)
+    # Production
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
     
