5bd09844bccc2df0aaaf87d68ed09a08bcd320e4
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2023-08-07 18:22:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = create_app()
 if __name__ == '__main__':
     # app.run(debug=True)
     # Production using wsgi server
-    app.run(port=5000)
+    app.run(host='0.0.0.0:$PORT',port=5000)
     
