8b17e8b1f0ac5a10073cc7bdd6f6761e376d0d91
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2023-08-07 19:53:39 -0400

    Updated auth.py

--- website/auth.py
+++ website/auth.py
@@ -11,6 +11,30 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        email = request.form.get('logemail')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        # Basic checks for the input information
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists :/', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        else:
+            new_user = User(email = email, firstName = first_name, password = generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+        
+        
+        
+        
+        
         email = request.form.get('email')
         password = request.form.get('password')
         
@@ -35,6 +59,7 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
+"""
 # create a route for the sign up page
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -60,3 +85,4 @@ def sign_up():
             return redirect(url_for('views.home'))
         
     return render_template("sign_up.html", user=current_user)
+"""
