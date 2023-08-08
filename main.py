d19377c817e5305d9e3652b953f69f10f24a922e
bjRichardLiu <102811722+bjRichardLiu@users.noreply.github.com>
2023-08-08 00:28:16 -0400

    Updated for deployment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
     # Production using wsgi server
-    # app.run(host='0.0.0.0:$PORT',port=5000)
+    app.run(host='0.0.0.0:$PORT',port=5000)
     
